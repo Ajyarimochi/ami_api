e263be71b273f6bd842b331c539f344f4390fd36
ura <ura@example.com>
2019-10-28 12:52:15 +0900

    Conect SQLite and Control Response data by Response data.

--- coupon/views.py
+++ coupon/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Coupon #Couponクラスをインポート
+from django.db.models import Q
+import json
 
-# Create your views here.
-def coupon(request):
-    if 'coupon_code' in request.GET:
-        coupon_code = request.GET['coupon_code']
-        if coupon_code == '0001':
-            benefit = '1000円引きクーポン！'
-            deadline = '2019/10/31'
-            message = ''
-        elif coupon_code == '0002':
-            benefit = '10%引きクーポン！'
-            deadline = '2019/11/30'
-            message = ''
-        else:
-            benefit = 'NA'
-            deadline = 'NA'
-            message = '利用可能なクーポンが見つかりません'
-        
+def coupon(request): 
+
+    coupons = [] #複数のレコードを辞書型として格納するための配列を用意
+    if 'coupon_store' in request.GET: #リクエストパラメータで店舗を指定された場合の処理
+        coupon_store = request.GET['coupon_store']
+        #data = Coupon.objects.filter(store=coupon_store) #リクエストされた店舗と全店で使えるクーポンを取得
+        data = Coupon.objects.filter(Q(store=coupon_store) | Q(store='全店')) #リクエストされた店舗と全店で使えるクーポンを取得
+    else: #リクエストパラメータが無い場合は全てのクーポンを返す
+        data = Coupon.objects.all()
+
+    for record in data: #for文を使い1レコードずつ辞書型に変換
         params = {
-            'coupon_code':coupon_code,
-            'coupon_benefits':benefit,
-            'coupon_deadline':deadline,
-            'message':message,
-        }
-        return render(request, 'coupon/index.html', params)
+            'coupon_code':str(record.code), #個々のレコードはdata型なのでString型にキャストする
+            'coupon_benefits':str(record.benefit),
+            'coupon_explanation':str(record.explanation),
+            'coupon_store':str(record.store),
+            'coupon_start':str(record.start),
+            'coupon_deadline':str(record.deadline),
+            'coupon_status':str(record.status),
+            }
+        coupons.append(params) #辞書型にしたレコードを配列に格納
 
+    json_str = json.dumps(coupons, ensure_ascii=False, indent=2) #辞書型にした複数のレコードを格納した配列を渡す
+    return HttpResponse(json_str)
