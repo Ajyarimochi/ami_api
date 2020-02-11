9f16ac55e65217b58420cde10c8283e3e4433530
ura <ura@example.com>
2020-02-11 19:10:19 +0900

    Finish develop to get coupon image urls.

--- coupon/models.py
+++ coupon/models.py
@@ -5,6 +5,7 @@ class Coupon(models.Model):
     code = models.CharField(max_length=20)
     benefit = models.CharField(max_length=1000)
     explanation = models.CharField(max_length=2000)
+    image = models.CharField(max_length=500, null=True, blank=True) # 画像URL格納用に追加
     store = models.CharField(max_length=1000)
     start = models.DateField()
     deadline = models.DateField()
@@ -12,7 +13,3 @@ class Coupon(models.Model):
 
     def __str__(self):
         return '<Coupon:id=' + str(self.id) + ',' + self.code + ',' + self.benefit + '>'
-
-
-
-
